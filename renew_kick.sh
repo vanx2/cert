b6e7a0d955cc14e3ee167ffbd8782b786e9c5913
Shigeto Yatani (for work) <vanxx2@gmail.com>
2023-05-22 17:53:57 +0900

    Update renew_kick.sh

--- renew_kick.sh
+++ renew_kick.sh
@@ -1,4 +1,4 @@
-#!/bin/bashA
+#!/bin/bash
 date
 self_dir=$(cd $(dirname $0); pwd)
 sudo /usr/local/bin/certbot renew --manual-auth-hook $self_dir/renew_auth.sh --manual-cleanup-hook $self_dir/renew_clean.sh
